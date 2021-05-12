f6585fa7d382ffbb136c151ac52ed7de5ee42b6c
Abderraouf Saoudi <ha_saoudi@esi.dz>
2021-05-12 07:52:37 +0100

    Updating login activity design

--- app/src/main/java/com/clovertech/autolibdz/LoginActivity.kt
+++ app/src/main/java/com/clovertech/autolibdz/LoginActivity.kt
@@ -11,12 +11,20 @@ class LoginActivity : AppCompatActivity() , View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_btn.setOnClickListener(this)
+        forgotPassword_txt_view.setOnClickListener(this)
         register_txt_view.setOnClickListener(this)
 
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
+            R.id.login_btn -> {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+            R.id.forgotPassword_txt_view -> {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
             R.id.register_txt_view -> {
                 startActivity(Intent(this, RegisterActivity::class.java))
             }

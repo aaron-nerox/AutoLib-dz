3fa79b957bc7ee79d290b1ba58d63dd1441334d4
Meziane Dahou <hd_meziane@esi.dz>
2021-05-18 04:33:59 +0100

    init drawable

---
+++ app/src/main/java/com/clovertech/autolibdz/ui/ProfileAct.kt
@@ -0,0 +1,11 @@
+package com.clovertech.autolibdz.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

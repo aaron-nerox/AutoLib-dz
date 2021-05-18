6280cb2826dbcdd323d46e1707ce2cb395c89460
Meziane Dahou <hd_meziane@esi.dz>
2021-05-18 05:05:34 +0100

    report a problem layout added

---
+++ app/src/main/java/com/clovertech/autolibdz/ui/SignalAct.kt
@@ -0,0 +1,12 @@
+package com.clovertech.autolibdz.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.clovertech.autolibdz.R
+
+class SignalAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signal)
+    }
+}

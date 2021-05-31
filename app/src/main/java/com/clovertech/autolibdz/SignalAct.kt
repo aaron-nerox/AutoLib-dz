1659f8b105e7c6ba0e403fffe730c941c5d309a1
Meziane Dahou <hd_meziane@esi.dz>
2021-05-31 09:31:59 +0100

    signal activity added

---
+++ app/src/main/java/com/clovertech/autolibdz/SignalAct.kt
@@ -0,0 +1,11 @@
+package com.clovertech.autolibdz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignalAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signal)
+    }
+}

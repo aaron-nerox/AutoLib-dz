d3829ce98d74a6196ae3bbd3f3818124b9bdcba5
celineserb <38558430+celineserb@users.noreply.github.com>
2021-06-09 11:08:32 +0100

    Revert "Send tenant card"

--- app/src/main/java/com/clovertech/autolibdz/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.clovertech.autolibdz.utils
-
-class Constants {
-    companion object{
-        const val Cars_BASE_URL = "http://54.37.87.85:5000/pricing/"
-        const val Pay_BASE_URL="http://54.37.87.85:8000/payment/cards/"
-
-    }
-}
